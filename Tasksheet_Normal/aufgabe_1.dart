8806d819f2ca7ed1a4cf33392d14450c485eeaa8
Shokri Francis Raoof <raoofs.de@gmail.com>
2025-03-13 10:43:19 +0100

    first commit

---
+++ Tasksheet_Normal/aufgabe_1.dart
@@ -0,0 +1,13 @@
+int summe(int a, int b, int c) {
+  return a + b + c;
+}
+
+void main() {
+  int numSum1 = summe(5, 6, 45);
+  int numSum2 = summe(-5, 8, 6);
+  int numSum3 = summe(85, 9546, 0);
+  print(numSum1);
+  print(numSum2);
+  print(numSum3);
+
+}
