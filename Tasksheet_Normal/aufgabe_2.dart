96cb42f26dec95d751d9e385664c9ba39aa94169
Shokri Francis Raoof <raoofs.de@gmail.com>
2025-03-13 10:48:05 +0100

    added aufgabe 2 fertig

---
+++ Tasksheet_Normal/aufgabe_2.dart
@@ -0,0 +1,14 @@
+int showTextLength(String text) {
+  return text.length;
+
+}
+
+void main() {
+  int textLength1 = showTextLength("Handcreme");
+  int textLength2 = showTextLength("Wohnung");
+  int textLength3 = showTextLength("Macbook");
+
+  print(textLength1);
+  print(textLength2);
+  print(textLength3);
+}
